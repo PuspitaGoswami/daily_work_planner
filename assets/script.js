0b5a29406694ec1602796fdbbc8de19bd36f4527
Your Name <you@example.com>
2023-03-05 00:40:19 +0000

    Add color for different time blocks

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,22 @@
-$(document).ready(function() {
-    var currentDate = moment().format("dddd, MMMM Do");
-    $("#currentDay").text(currentDate);
+$(document).ready(function () {
+  var currentDate = moment().format("dddd, MMMM Do");
+  $("#currentDay").text(currentDate);
 
+  // Get the current hour using moment.js
+  var currentHour = moment().hours();
 
+  // Loop over each time block
+  $(".time-block").each(function () {
+    // Get the hour for this time block by parsing the text of the span element
+    var blockHour = parseInt($(this).find(".hour").text().trim().split(" ")[0]);
+
+    // Add the appropriate class to the time block based on whether it represents a past, present, or future hour
+    if (blockHour < currentHour) {
+      $(this).addClass("past");
+    } else if (blockHour === currentHour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("future");
+    }
   });
-  
+});
